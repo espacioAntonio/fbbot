b8f7f9e5320b03396ed76f52ce925496e31fdd9a
espacioAntonio <acruz@metagraphos.com>
2017-08-21 17:44:31 -0500

    simpleEchoBot.py with wrapper arguments

--- fbbot/test/simpleEchoBot.py
+++ fbbot/test/simpleEchoBot.py
@@ -10,58 +10,69 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
 from fbbot.bot import Bot
 
-FB_VERIFY_TOKEN = "VERIFY_TOKEN_DEFINED_BY_DEVELOPER"
-FB_PAGE_TOKEN = "FACEBOOK_PAGE_TOKEN"
-BASE_URL = "URL_PROVIDED_FOR_NGROK" #example: https://12346578.ngrok.io or https://www.yourdomain.com
+#FB_VERIFY_TOKEN = "VERIFY_TOKEN_DEFINED_BY_DEVELOPER"
+#FB_PAGE_TOKEN = "FACEBOOK_PAGE_TOKEN"
+#BASE_URL = "URL_PROVIDED_FOR_NGROK" #example: https://12346578.ngrok.io or https://www.yourdomain.com
 
 
-# HTTPRequestHandler class
-class TestRequestHandler(BaseHTTPRequestHandler):
-    myBot = Bot(FB_PAGE_TOKEN, BASE_URL)
+def make_handler_with_tokens(verify_token, page_token, base_url, bot=None):
+    # HTTPRequestHandler class
+    class TestRequestHandler(BaseHTTPRequestHandler):
+        if bot:
+            myBot = bot
+        else:
+            myBot = Bot(page_token, base_url)
 
-    def do_GET(self):
-        print(self.path)
-        message = "default message"
-        if self.path.startswith('/webhook'):
-            query = urlparse(self.path).query
-            query = dict(qc.split("=") for qc in query.split("&"))
-            if 'hub.verify_token' in query and query['hub.verify_token'] == FB_VERIFY_TOKEN:
-                message = query['hub.challenge']
-            else:
-                message = "Hello World, webhook enable"
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(bytes(message, "utf8"))
-        return
+        def do_GET(self):
+            print(self.path)
+            message = "default message"
+            if self.path.startswith('/webhook'):
+                query = urlparse(self.path).query
+                try:
+                    query = dict(qc.split("=") for qc in query.split("&"))
+                    if 'hub.verify_token' in query and query['hub.verify_token'] == verify_token:
+                        message = query['hub.challenge']
+                    else:
+                        message = "Hello World, webhook enable"
+                except Exception as e:
+                    message = "Hello World, webhook enable :: ERROR :: " + repr(e)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes(message, "utf8"))
+            return
 
-    def do_POST(self):
-        if self.path.startswith('/webhook'):
-            content_len = int(self.headers.get('Content-Length'))
-            post_body = self.rfile.read(content_len)
-            pprint(":: post_body ::")
-            pprint(post_body)
-            incoming_message = json.loads(str(post_body, 'utf-8'))
-            if 'object' in incoming_message and incoming_message['object'] == 'page':
-                for entry in incoming_message['entry']:
-                    for message in entry['messaging']:
-                        pprint(":: message ::")
-                        pprint(message)
-                        for message_type in self.myBot.message_type_functions:
-                            if message_type in message:
-                                self.myBot.message_type_functions[message_type](message)
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(bytes("ok", "utf8"))
-        return
+        def do_POST(self):
+            if self.path.startswith('/webhook'):
+                content_len = int(self.headers.get('Content-Length'))
+                post_body = self.rfile.read(content_len)
+                pprint(":: post_body ::")
+                pprint(post_body)
+                incoming_message = json.loads(str(post_body, 'utf-8'))
+                if 'object' in incoming_message and incoming_message['object'] == 'page':
+                    for entry in incoming_message['entry']:
+                        for message in entry['messaging']:
+                            pprint(":: message ::")
+                            pprint(message)
+                            for message_type in self.myBot.message_type_functions:
+                                if message_type in message:
+                                    self.myBot.message_type_functions[message_type](message)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes("ok", "utf8"))
+            return
+    return TestRequestHandler
 
 
-def run():
+def run(verify_token=None, page_token=None, base_url=None):
+    if not verify_token:
+        raise KeyError("'verify_token' is required: 'VERIFY_TOKEN_DEFINED_BY_DEVELOPER'")
+    if not page_token:
+        raise KeyError("'page_token' is required: 'FACEBOOK_PAGE_TOKEN'")
     print('starting server...')
     server_address = ('127.0.0.1', 8080)
-    httpd = HTTPServer(server_address, TestRequestHandler)
+    request_handler = make_handler_with_tokens(verify_token, page_token, base_url)
+    httpd = HTTPServer(server_address, request_handler)
     print('running server...')
     httpd.serve_forever()
-
-run()
